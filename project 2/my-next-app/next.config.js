c3c120f3e44c5422a768e0d30823f9074fc30ff7
gokuls-bit <gokulsant684@gmail.com>
2025-06-10 01:21:14 +0530

    Initial push to mini-projects-2nd-collection

--- project 2/my-next-app/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: ['http://192.168.1.4:3000'],
-};
-
-module.exports = nextConfig;
-
--- project 2/my-next-app/src/app/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import 'leaflet/dist/leaflet.css';
-import '@/styles/globals.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
