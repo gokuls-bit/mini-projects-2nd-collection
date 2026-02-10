087f871bd3631cae7d9cc4f63793a90d53cb25bb
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-02-10 22:25:11 +0530

    Uncomment example domain in next.config.js

--- nexttryportfoliotypescript/next.config.js
+++ nexttryportfoliotypescript/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     // Add domains if you're loading images from external sources (e.g., a CDN or CMS)
     // For local images, you generally don't need to specify domains, but if you run into issues,
     // ensure your local paths are correct.
-    // domains: ['example.com'], // Example: if you fetch project images from a specific domain
+    // domains: ['example.com'], // Example: if you fetch project images from a specific domain 
     formats: ['image/avif', 'image/webp'], // Optimize image formats
     minimumCacheTTL: 60, // seconds
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
